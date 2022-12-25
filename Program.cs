662ba87978b4c708f6fc7bbe2be760e7c0738810
AbdelrahmanElmongy1 <121382605+AbdelrahmanElmongy1@users.noreply.github.com>
2022-12-25 13:56:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("n1");
+            int Start = int.Parse(Console.ReadLine());
+            Console.WriteLine("n2");
+
+            int End = int.Parse(Console.ReadLine());
+
+            int Sum = 0;
+            for (int i = Start; i <= End; i++) {
+                for (int x = 1; x <= i; x++){
+                    if(i%x==0)
+                    Sum++;
+                }
+                if(Sum==2){
+                    Console.WriteLine(i);
+                    Sum=0;
+                }else{
+                    Sum=0;
+                    continue;
+            }
+               
+
+               
+                }
+            
+            
+            
+            }
+            
+
+       
+            
+
+
+        }
+    }
+
